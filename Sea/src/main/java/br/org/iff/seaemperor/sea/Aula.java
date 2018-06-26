71fca8cc7b11bfe2875ebae06b2a3f517ec761cf
PlayerUnnamed <mateusgv21@gmail.com>
2018-06-26 10:21:07 -0300

    o site todo

---
+++ Sea/src/main/java/br/org/iff/seaemperor/sea/Aula.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.iff.seaemperor.sea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Aula {
+
+
+    public static void main(String[] args) {
+        System.out.println("Olá Brian");
+	int x = 44;
+	double psilone = 4.7,  xlone = 17.9;
+    	boolean Vingadoresephoda = true;
+    	String palavra = "facinho",frase = "java é "+palavra;
+    	System.out.println(frase);
+           	 
+    	System.out.println(" O sentido da vida é " +x + " De acordo com o mochileiro das galáxias");
+           	 
+	}
+	public static void Leia() throws IOException{                       	 
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter an integer");
+	String c = br.readLine();
+    
+    
+}
+    
+    
+ 
+    
+}
+
+    }
+
+    
+}
+
---
+++ Sea/src/main/java/br/org/iff/seaemperor/sea/Emperor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.iff.seaemperor.sea;
+
+/**
+ *
+ * @author aluno
+ */
+public class Emperor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá, Galera!!!\njava é muito legal =D");
+        int x = 42;
+        System.out.println("o sentido da vida, do universo e tudo o mais é "+ x +".");
+        
+    }
+    
+}
